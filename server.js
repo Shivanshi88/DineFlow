420ba36d2f19d8d2ec726ed4459ba96f0e644ee7
shivanshi sahu <shivanshisahu88@gmail.com>
2026-01-05 11:35:23 +0530

    Add root route and confirm backend structure

--- server.js
+++ server.js
@@ -35,3 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
+
